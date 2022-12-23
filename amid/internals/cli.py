dd41f39972806d860cee7d05a58e6e0606580172
Max <maxs987@gmail.com>
2022-12-23 12:01:52 +0300

    codestyle

--- amid/internals/cli.py
+++ amid/internals/cli.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 import typer
-from bev.cli.init import init as bev_init
 from bev.cli.app import command
+from bev.cli.init import init as bev_init
 
 from .registry import gather_datasets
+
 
 app = typer.Typer()
 
@@ -15,26 +16,33 @@ def main():
 
 @command(app)
 def init(
-        permissions: str = typer.Option(
-            None, '--permissions', '-p', help='The permissions mask used to create the storage, e.g. 770',
-        ),
-        group: str = typer.Option(
-            None, '--group', '-g', help='The group used to create the storage',
-        ),
+    permissions: str = typer.Option(
+        None,
+        '--permissions',
+        '-p',
+        help='The permissions mask used to create the storage, e.g. 770',
+    ),
+    group: str = typer.Option(
+        None,
+        '--group',
+        '-g',
+        help='The group used to create the storage',
+    ),
 ):
     return bev_init(Path(__file__).resolve().parent.parent / 'data', permissions, group)
 
 
 @command(app)
 def populate(
-        dataset: str = typer.Argument(..., help='The dataset to populate'),
-        root: Path = typer.Argument(..., help='The path to the downloaded raw data'),
-        ignore_errors: bool = typer.Option(
-            False, help='Whether to ignore all the exception during population. '
-                        'Warning! The failed ids will be excluded from the populated dataset'
-        ),
-        n_jobs: int = typer.Option(1, help='How many threads to use for population'),
-        fetch: bool = typer.Option(True, help='Whether to fetch the missing data from remote locations, if any'),
+    dataset: str = typer.Argument(..., help='The dataset to populate'),
+    root: Path = typer.Argument(..., help='The path to the downloaded raw data'),
+    ignore_errors: bool = typer.Option(
+        False,
+        help='Whether to ignore all the exception during population. '
+        'Warning! The failed ids will be excluded from the populated dataset',
+    ),
+    n_jobs: int = typer.Option(1, help='How many threads to use for population'),
+    fetch: bool = typer.Option(True, help='Whether to fetch the missing data from remote locations, if any'),
 ):
     cls = gather_datasets()[dataset][0]
     ds = cls(root=root)
