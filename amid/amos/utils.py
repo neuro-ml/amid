60eadd9cbe2bb035ef011f70694c43ec2b311065
Alexey Shevtsov <a.shevtsov@ira-labs.com>
2022-12-31 09:02:41 +0300

    populated

--- amid/amos/utils.py
+++ amid/amos/utils.py
@@ -4,6 +4,10 @@ from .const import COLUMN2LABEL
 def add_labels(scope):
     def make_loader(column):
         def loader(i, _meta):
+            # ambigous data in meta
+            if int(i) in [500, 600]:
+                return None
+
             return _meta[_meta['amos_id'] == int(i)][column].item()
 
         return loader
