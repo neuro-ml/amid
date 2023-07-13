6bcabc23f3f4ab4f087529a931a44e9068c0d56d
kurmukovai <kurmukovai@gmail.com>
2023-07-13 07:35:58 +0300

    add deeplesion

--- amid/deeplesion.py
+++ amid/deeplesion.py
@@ -1,12 +1,11 @@
 # TODO: Add annotation info from DL_info.csv
 
+from functools import lru_cache
 from pathlib import Path
 
-import nibabel
 import deli
+import nibabel
 import numpy as np
-
-from functools import lru_cache
 from connectome import Source, meta
 from connectome.interface.nodes import Silent
 
@@ -15,39 +14,39 @@ from .internals import checksum, register
 
 @register(
     body_region=('Abdomen', 'Thorax'),
-    license='DeepLesion data license', # TODO
+    license='DeepLesion data license',  # TODO
     link='https://nihcc.app.box.com/v/DeepLesion',
     modality="CT",
     prep_data_size='259G',
     raw_data_size='259G',
-    task=('Localisation','Detection','Classification'),
+    task=('Localisation', 'Detection', 'Classification'),
 )
 @checksum('deeplesion')
 class DeepLesion(Source):
     """
     DeepLesion is composed of 33,688 bookmarked radiology images from
     10,825 studies of 4,477 unique patients. For every bookmarked image, a bound-
-    ing box is created to cover the target lesion based on its measured diameters [1]. 
+    ing box is created to cover the target lesion based on its measured diameters [1].
 
     Parameters
     ----------
     root : str, Path, optional
         path to the folder containing `DL_info.csv` file and a subfolder `Images_nifti` with 20094 nii.gz files.
- 
+
     Notes
     -----
-    Dataset is available at https://nihcc.app.box.com/v/DeepLesion 
+    Dataset is available at https://nihcc.app.box.com/v/DeepLesion
 
     To download the data we recommend using a Python script provided by the authors `batch_download_zips.py`.
     Once you download the data and unarchive all 56 zip archives, you should run `DL_save_nifti.py` provided by the authors
     to convert 2D PNGs into 20094 nii.gz files.
-    
+
     Example
     --------
     >>> ds = DeepLesion(root='/path/to/folder')
     >>> print(len(ds.ids))
     # 20094
-    
+
     References
     ----------
     .. [1] Yan, Ke, Xiaosong Wang, Le Lu, and Ronald M. Summers. "Deeplesion: Automated deep mining,
@@ -69,15 +68,15 @@ class DeepLesion(Source):
 
     def _image_file(i, _base):
         return nibabel.load(_base / "Images_nifti" / f"{i}.nii.gz")
-    
+
     @lru_cache
     def _metadata(_base):
         return deli.load(_base / "DL_info.csv")
-    
+
     def _row(i, _metadata):
         patient, study, series = map(int, i.split("_")[:3])
         return _metadata.query("Patient_index==@patient").query("Study_index==@study").query("Series_ID==@series")
-         
+
     def patient_id(i):
         patient, study, series = map(int, i.split("_")[:3])
         return patient
@@ -96,7 +95,7 @@ class DeepLesion(Source):
     def age(_row):
         """Patient Age might be different for different studies (dataset contains longitudinal records)."""
         return _row.Patient_age.iloc[0]
-        
+
     def ct_window(_row):
         return _row.DICOM_windows.iloc[0]
 
@@ -108,6 +107,6 @@ class DeepLesion(Source):
 
     def image(_image_file):
         return np.asarray(_image_file.dataobj)
-    
+
     def train_val_fold(_row):
         return int(_row.Train_Val_Test.iloc[0])
