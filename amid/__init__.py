1e4953eb02f15fbc76cd7f1a143b5d8afd3db113
kurmukovai <kurmukovai@gmail.com>
2023-07-21 17:12:59 +0300

    add dataset imports

--- amid/__init__.py
+++ amid/__init__.py
@@ -3,5 +3,32 @@ from connectome.cache import unstable_module
 from .__version__ import __version__
 from .internals import CacheColumns, CacheToDisk
 
+from bimcv import BIMCVCovid19
+from brats2021 import BraTS2021
+from cl_detection import CLDetection2023
+from covid_1110 import MoscowCovid1110
+from crlm import CRLM
+from crossmoda import CrossMoDA
+from ct_ich import CT_ICH
+from deeplesion import DeepLesion
+from egd import EGD
+from flare2022 import FLARE2022
+from liver_medseg import LiverMedseg
+from medseg9 import Medseg9
+from midrc import MIDRC
+from mood import MOOD
+from nlst import NLST
+from nsclc import NSCLC
+from stanford_coca import StanfordCoCa
+from verse import VerSe
+from amos import AMOS
+from cancer_500 import MoscowCancer500
+from cc359 import CC359
+from lidc import LIDC
+from lits import LiTS
+from rsna_bc import RSNABreastCancer
+from totalsegmentator import Totalsegmentator
+from upenn_gbm import UPENN_GBM
+from vs_seg import VSSEG
 
 unstable_module(__name__)
