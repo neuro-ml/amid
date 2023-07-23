ba9810a90a818336a537451a4ebdde49549f9424
kurmukovai <kurmukovai@gmail.com>
2023-07-23 13:00:33 +0300

    actually fixed the imports

--- amid/__init__.py
+++ amid/__init__.py
@@ -1,34 +1,34 @@
-from bimcv import BIMCVCovid19
-from brats2021 import BraTS2021
-from cl_detection import CLDetection2023
 from connectome.cache import unstable_module
-from covid_1110 import MoscowCovid1110
-from crlm import CRLM
-from crossmoda import CrossMoDA
-from ct_ich import CT_ICH
-from deeplesion import DeepLesion
-from egd import EGD
-from flare2022 import FLARE2022
-from liver_medseg import LiverMedseg
-from medseg9 import Medseg9
-from midrc import MIDRC
-from mood import MOOD
-from nlst import NLST
-from nsclc import NSCLC
-from stanford_coca import StanfordCoCa
-from verse import VerSe
-from vs_seg import VSSEG
 
 from .__version__ import __version__
 from .amos import AMOS
+from .bimcv import BIMCVCovid19
+from .brats2021 import BraTS2021
 from .cancer_500 import MoscowCancer500
 from .cc359 import CC359
+from .cl_detection import CLDetection2023
+from .covid_1110 import MoscowCovid1110
+from .crlm import CRLM
+from .crossmoda import CrossMoDA
+from .ct_ich import CT_ICH
+from .deeplesion import DeepLesion
+from .egd import EGD
+from .flare2022 import FLARE2022
 from .internals import CacheColumns, CacheToDisk
 from .lidc import LIDC
 from .lits import LiTS
+from .liver_medseg import LiverMedseg
+from .medseg9 import Medseg9
+from .midrc import MIDRC
+from .mood import MOOD
+from .nlst import NLST
+from .nsclc import NSCLC
 from .rsna_bc import RSNABreastCancer
+from .stanford_coca import StanfordCoCa
 from .totalsegmentator import Totalsegmentator
 from .upenn_gbm import UPENN_GBM
+from .verse import VerSe
+from .vs_seg import VSSEG
 
 
 unstable_module(__name__)
