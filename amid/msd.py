abeb60c89c7163ccf8fef07a2919383c6408c4b4
flomakin <f.lomakin@ira-labs.com>
2023-08-28 18:17:12 +0300

    improvements

--- amid/msd.py
+++ amid/msd.py
@@ -11,6 +11,8 @@ import contextlib
 import tarfile
 from pathlib import Path
 import gzip
+import os
+import json
 
 import nibabel as nb
 import numpy as np
@@ -18,23 +20,22 @@ from connectome import Output, Source, meta, Transform
 from connectome.interface.nodes import Silent
 
 from .internals import checksum, licenses, register
-from .utils import deprecate
 
 
 @register(
-    body_region='Abdominal',
+    body_region=('Chest', 'Abdominal', 'Head'),
     license=licenses.CC_BYSA_40,
-    link='https://msd-for-monai.s3-us-west-2.amazonaws.com/Task03_Liver.tar',
-    modality='CT',
-    # prep_data_size='',
-    raw_data_size='26.9G',
-    task='Liver and tumour segmentation',
+    # There is a link to AWS, I don't know which is better
+    link='https://drive.google.com/drive/folders/1HqEgzS8BV2c7xYNrZdEAnrHk7osJJ--2',
+    modality=('CT', 'MRI', 'MRI FLAIR', 'MRI T1w', 'MRI t1gd', 'MRI T2w', 'MRI T2', 'MRI ADC'),
+    raw_data_size='97.8G',
+    task='Medical Segmentation Decathlon',
 )
 
 @checksum('msd')
-class Task03Liver(Source):
+class MSD(Source):
     """
-    Task03Liver is a public liver and tumour  CT segmentation dataset from Medical Segmentaton Decathlon Challenge with 130 annotated images.
+    MSD is a Medical Segmentaton Decathlon Challenge with 10 tasks.
     Parameters
     ----------
     root : str, Path, optional
@@ -44,65 +45,65 @@ class Task03Liver(Source):
         the data version. Only has effect if the library was installed from a cloned git repository.
     Notes
     -----
-    Data can be downloaded here: https://msd-for-monai.s3-us-west-2.amazonaws.com/Task03_Liver.tar.
+    Data can be downloaded here: https://msd-for-monai.s3-us-west-2.amazonaws.com/
+    or here: https://drive.google.com/drive/folders/1HqEgzS8BV2c7xYNrZdEAnrHk7osJJ--2/
     Then, the folder with raw downloaded data should contain tar archive with data and masks
     (`Task03_Liver.tar`).
     """
 
     _root: str = None
+    _task_to_name: dict = {"Task01_BrainTumour": "BRATS",
+                            "Task02_Heart": "heart",
+                            "Task03_Liver": "liver",
+                            "Task04_Hippocampus": "hippocampus",
+                            "Task05_Prostate": "prostate",
+                            "Task06_Lung": "lung",
+                            "Task07_Pancreas": "pancreas",
+                            "Task08_HepaticVessel": "hepaticvessel",
+                            "Task09_Spleen": "spleen",
+                            "Task10_Colon": "colon",
+                            }
 
-#TODO adopt for all datasets in MSD
     @meta
-    def ids(_root: Silent):
+    def ids(_root: Silent) -> tuple: # _tasks):
         result = set()
-        tasks = ("Task03_Liver.tar", ...)
-        for task in tasks:
 
-            with tarfile.open(Path(_root) / task) as tf:
+        tar_files = [os.path.join(_root, f) for f in os.listdir(_root) if f.endswith('.tar')]
+        for tar_file in tar_files:
+            task = str(tar_file).split('/')[-1].split('.')[0]
+            with tarfile.open(tar_file, 'r') as tf:
+
+        # for task in _tasks.keys():
+        #     with tarfile.open(Path(_root) / f"{task}.tar") as tf:
+
                 for tarinfo in tf.getmembers():
                     if tarinfo.isdir():
                         continue
-                    fold = 'train_'
+                    fold = '_train_'
                     if 'Ts' in tarinfo.path:
-                        fold = 'test_'
+                        fold = '_test_'
                     file_stem = Path(tarinfo.path).stem
                     if file_stem.startswith('.') or not file_stem.endswith('.nii'):
                         continue
-                    result.add(fold + file_stem.split('.nii')[0])
 
-        return tuple(sorted(result))
-    
-    def train_test(i):
+                    result.add(task + fold + file_stem.split('_')[1].split('.')[0])
+
+        return tuple(sorted(result, key=lambda x: (x.split('_')[0], int(x.split('_')[-1]))))
+
+
+    def train_test(i) -> str:
         if "train" in i: return "train"
         else: return "test"
 
-#TODO from json
-    def image_modality(i) -> str:
-        None
-
-# TODO
     def task(i) -> str:
-        task_dict = {'liver':"Task03_Liver",
-         'spleen':....}
-        
-        for key,value in task_dict.items():
-            if key in i:
-                return  value
-            
-
-#TODO from json
-    def segmentation_labels(i) -> dict:
-        """Returns segmentation labels for the task
-        """
-        return 
-
-#TODO use self.task instead of hardcoded  'Task03_Liver'
-    def _file(i, task, _root: Silent):
+        return "_".join(i.split("_")[:2])
+    
+    # def _file(i, task, _root: Silent, _task_to_name): swears at it
+    def _file(i, _root: Silent, _task_to_name):
+        task = "_".join(i.split("_")[:2])
+        name = _task_to_name[task]
         num_id = i.split('_')[-1]
-        if 'train' in i:
-            return Path(_root) / task / 'imagesTr'/ f'liver_{num_id}.nii.gz'
-        else:
-            return Path(_root) / 'Task03_Liver'/ 'imagesTs'/ f'liver_{num_id}.nii.gz'
+        return Path(_root) / task / ('imagesTr' if 'train' in i else 'imagesTs') / f'{name}_{num_id}.nii.gz'
 
     def image(_file):
         with open_nii_gz_file(_file) as nii_image:
@@ -120,8 +121,23 @@ class Task03Liver(Source):
         """The 4x4 matrix that gives the image's spatial orientation."""
         with open_nii_gz_file(_file) as nii_image:
             return nii_image.affine
+        
 
-    
+    def image_modality(i, _root: Silent) -> str:
+        task = "_".join(i.split("_")[:2])
+        with tarfile.open(Path(_root) / f"{task}.tar") as tf:
+            member = tf.getmember(f"{task}/dataset.json")
+            file = tf.extractfile(member)
+            return json.loads(file.read())["modality"]
+
+    def segmentation_labels(i, _root: Silent) -> dict:
+        """Returns segmentation labels for the task"""
+        task = "_".join(i.split("_")[:2])
+        with tarfile.open(Path(_root) / f"{task}.tar") as tf:
+            member = tf.getmember(f"{task}/dataset.json")
+            file = tf.extractfile(member)
+            return json.loads(file.read())["labels"]
+
     @classmethod
     def normalizer(cls):
         return SpacingFromAffine()
