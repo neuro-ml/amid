d373867044c3be752cad409b1d8ce23bb54e7153
Aleksey Leonov <a.leonov@ira-labs.com>
2023-09-14 12:58:10 +0300

    added ribseg  annotations

--- amid/ribfrac/dataset.py
+++ amid/ribfrac/dataset.py
@@ -1,9 +1,11 @@
+from functools import lru_cache
 from pathlib import Path
 
 import nibabel
 import numpy as np
 from connectome import Source, meta
 from connectome.interface.nodes import Silent
+from pandas import read_excel
 
 from ..internals import checksum, licenses, register
 
@@ -24,13 +26,16 @@ class RibFrac(Source):
     both clinical research for automatic rib fracture detection and diagnoses,
     and engineering research for 3D detection, segmentation and classification.
 
+    Annotations are from available RibSeg segmentation benchmark
+    (https://ribfrac.grand-challenge.org/). Dataset contains all 660 segmentaion
+    masks (seg) and centerlines (cl).
+
 
     Parameters
     ----------
     root : str, Path, optional
         path to the folder containing the raw downloaded archives.
         If not provided, the cache is assumed to be already populated.
-
 
     Notes
     -----
@@ -40,13 +45,29 @@ class RibFrac(Source):
     https://doi.org/10.5281/zenodo.3893495 -- val (80 images)
     https://zenodo.org/record/3993380 -- test (160 images without annotation)
 
+    Annotations downloaded from here:
+    At the time of September 2023 the dataset was pre-launched, for details go to https://github.com/M3DV/RibSeg
+    dataset: https://drive.google.com/file/d/1ZZGGrhd0y1fLyOZGo_Y-wlVUP4lkHVgm/view?usp=sharing
+    description table: https://docs.google.com/spreadsheets/d/1lz9liWPy8yHybKCdO3BCA9K76QH8a54XduiZS_9fK70/edit?usp=sharing
 
+    example:
+    seg: seg = nibabel.load(file name).get_fdata()
+    # seg is a np array / volume of (512,512,N) with rib labels
+    cl: cl = np.load(file name)['cl']
+    # cl is a np array of (24,500,3), each rib contains 500 points
 
     References
     ----------
+    Data:
     Jiancheng Yang, Liang Jin, Bingbing Ni, & Ming Li. (2020).
     RibFrac Dataset: A Benchmark for Rib Fracture Detection,
     Segmentation and Classification
+
+    Annotation:
+    Liang Jin, Shixuan Gu, Donglai Wei, Jason Ken Adhinarta,
+    Kaiming Kuang, Yongjie Jessica Zhang, et al (2022)
+    RibSeg v2: A Large-scale Benchmark for
+    Rib Labeling and Anatomical Centerline Extraction
     """
 
     _root: str
@@ -97,3 +118,18 @@ class RibFrac(Source):
         """The 4x4 matrix that gives the image's spatial orientation"""
         image_path = _id2folder[i] / f'{i}-image.nii.gz'
         return nibabel.load(image_path).affine
+
+    def seg(i, _base):
+        seg_path = _base / 'seg' / f'{i}-rib-seg.nii.gz'
+        ribs = nibabel.load(seg_path).get_fdata()
+        return ribs.astype(np.int16)
+
+    def cl(i, _base):
+        cl_path = _base / 'cl' / f'{i}.npz'
+        cl = np.load(cl_path)['cl']
+        return cl.astype(np.int16)
+
+    @lru_cache(None)
+    def _meta(_base):
+        file = 'RibSeg v2.xlsx'
+        return read_excel(_base / file)
