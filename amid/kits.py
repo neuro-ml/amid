f76da7c05f1a3c98d00c35fb5af690f926259f21
kurmukovai <kurmukovai@gmail.com>
2023-09-29 21:01:09 +0300

    fix masks, add mask label names

--- amid/kits.py
+++ amid/kits.py
@@ -8,6 +8,12 @@ from connectome.interface.nodes import Silent
 from .internals import normalize
 
 
+KITS_LABEL_NAMES = { # https://github.com/neheller/kits23/blob/063d4c00afd383fc68145a00c0aa6a4e2a3c0f50/kits23/configuration/labels.py#L23
+    1: "kidney",
+    2: "tumor",
+    3: "cyst"
+}
+
 class KiTS23Base(Source):
     """Kidney and Kidney Tumor Segmentation Challenge,
     The 2023 Kidney and Kidney Tumor Segmentation challenge (abbreviated KiTS23)
@@ -44,14 +50,18 @@ class KiTS23Base(Source):
     # TODO add multiple segmentations
     # TODO add labels mapping
     def mask(i, _root: Silent):
-        """ """
+        """Combined annotation for kidneys, tumor and cyst (if present)."""
         mask_path = Path(_root) / 'dataset' / i / 'segmentation.nii.gz'
         ct_scan_nifti = nb.load(mask_path)
-        return np.bool_(ct_scan_nifti.get_fdata()[...])
+        return np.int16(ct_scan_nifti.get_fdata())
 
     def affine(_image_file):
         """The 4x4 matrix that gives the image's spatial orientation."""
         return _image_file.affine
+    
+    def labels_names(i):
+        """Indicates which label correspond to which mask, consistent accross all samples."""
+        return KITS_LABEL_NAMES
 
 
 class SpacingFromAffine(Transform):
