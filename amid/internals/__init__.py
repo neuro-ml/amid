f20c25a35d42906d62ad238b3ef1cc3148f8f7b3
Max <max@ira-labs.com>
2024-06-23 18:33:38 +0100

    removed the checksum class

--- amid/internals/__init__.py
+++ amid/internals/__init__.py
@@ -1,5 +1,4 @@
 from .base import get_repo
 from .cache import CacheColumns, CacheToDisk
-from .checksum import CacheAndCheck
 from .dataset import Dataset, field
 from .registry import gather_datasets, register
